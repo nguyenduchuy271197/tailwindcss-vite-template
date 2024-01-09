e2cbeae91635b77507cec740117aad4ba8d0489a
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2024-01-09 21:21:35 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,142 @@
+export default function App() {
+  return (
+    <div>
+      <div className="max-w-screen-xl mx-auto">
+        <div className="grid grid-cols-3 gap-6">
+          <div className="bg-gray-100 p-4 rounded-xl">
+            <div className="flex gap-4">
+              <div>
+                <img
+                  src="https://starboard-one.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fjonathan.49b041d4.webp&w=3840&q=75"
+                  alt=""
+                  className="w-14 h-14 rounded-full object-cover object-top"
+                />
+              </div>
+              <div>
+                <h4 className="font-semibold text-gray-800">Jonathen tapas</h4>
+                <p className="text-gray-600 text-sm mt-0.5">
+                  Founder of saleMore
+                </p>
+              </div>
+            </div>
+            <div className="mt-6">
+              Great value and so easy to use and saves me so much time! I was
+              shocked by how much time and brain energy it saved me. Simple &
+              easy...gotta love that.
+            </div>
+          </div>
+          <div className="bg-gray-100 p-4 rounded-xl">
+            <div className="flex gap-4">
+              <div>
+                <img
+                  src="https://starboard-one.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fjonathan.49b041d4.webp&w=3840&q=75"
+                  alt=""
+                  className="w-14 h-14 rounded-full object-cover object-top"
+                />
+              </div>
+              <div>
+                <h4 className="font-semibold text-gray-800">Jonathen tapas</h4>
+                <p className="text-gray-600 text-sm mt-0.5">
+                  Founder of saleMore
+                </p>
+              </div>
+            </div>
+            <div className="mt-6">
+              Great value and so easy to use and saves me so much time! I was
+              shocked by how much time and brain energy it saved me. Simple &
+              easy...gotta love that.
+            </div>
+          </div>
+          <div className="bg-gray-100 p-4 rounded-xl">
+            <div className="flex gap-4">
+              <div>
+                <img
+                  src="https://starboard-one.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fjonathan.49b041d4.webp&w=3840&q=75"
+                  alt=""
+                  className="w-14 h-14 rounded-full object-cover object-top"
+                />
+              </div>
+              <div>
+                <h4 className="font-semibold text-gray-800">Jonathen tapas</h4>
+                <p className="text-gray-600 text-sm mt-0.5">
+                  Founder of saleMore
+                </p>
+              </div>
+            </div>
+            <div className="mt-6">
+              Great value and so easy to use and saves me so much time! I was
+              shocked by how much time and brain energy it saved me. Simple &
+              easy...gotta love that.
+            </div>
+          </div>
+          <div className="bg-gray-100 p-4 rounded-xl">
+            <div className="flex gap-4">
+              <div>
+                <img
+                  src="https://starboard-one.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fjonathan.49b041d4.webp&w=3840&q=75"
+                  alt=""
+                  className="w-14 h-14 rounded-full object-cover object-top"
+                />
+              </div>
+              <div>
+                <h4 className="font-semibold text-gray-800">Jonathen tapas</h4>
+                <p className="text-gray-600 text-sm mt-0.5">
+                  Founder of saleMore
+                </p>
+              </div>
+            </div>
+            <div className="mt-6">
+              Great value and so easy to use and saves me so much time! I was
+              shocked by how much time and brain energy it saved me. Simple &
+              easy...gotta love that.
+            </div>
+          </div>
+          <div className="bg-gray-100 p-4 rounded-xl">
+            <div className="flex gap-4">
+              <div>
+                <img
+                  src="https://starboard-one.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fjonathan.49b041d4.webp&w=3840&q=75"
+                  alt=""
+                  className="w-14 h-14 rounded-full object-cover object-top"
+                />
+              </div>
+              <div>
+                <h4 className="font-semibold text-gray-800">Jonathen tapas</h4>
+                <p className="text-gray-600 text-sm mt-0.5">
+                  Founder of saleMore
+                </p>
+              </div>
+            </div>
+            <div className="mt-6">
+              Great value and so easy to use and saves me so much time! I was
+              shocked by how much time and brain energy it saved me. Simple &
+              easy...gotta love that.
+            </div>
+          </div>
+          <div className="bg-gray-100 p-4 rounded-xl">
+            <div className="flex gap-4">
+              <div>
+                <img
+                  src="https://starboard-one.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fjonathan.49b041d4.webp&w=3840&q=75"
+                  alt=""
+                  className="w-14 h-14 rounded-full object-cover object-top"
+                />
+              </div>
+              <div>
+                <h4 className="font-semibold text-gray-800">Jonathen tapas</h4>
+                <p className="text-gray-600 text-sm mt-0.5">
+                  Founder of saleMore
+                </p>
+              </div>
+            </div>
+            <div className="mt-6">
+              Great value and so easy to use and saves me so much time! I was
+              shocked by how much time and brain energy it saved me. Simple &
+              easy...gotta love that.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
